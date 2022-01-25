ed9bbd60993c95eb261b1176c2ab3b9ebd1931a1
Tanvi Naik <naiktanvi30@gmail.com>
2022-01-25 12:29:43 +0530

    fixed smtp

--- main.py
+++ main.py
@@ -1,74 +1,33 @@
 from flask import Flask, render_template, request, url_for
-import smtplib
-from flask_mail import Mail, Message
-import socket
-from email.message import EmailMessage
+from smtplib import SMTP
 
 app = Flask(__name__)
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'jaggudixit686@gmail.com'
-app.config['MAIL_PASSWORD'] = 'jaggu@5252'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-# socket.getaddrinfo('127.0.0.1', 5000)
-
-mail = Mail(app)
-
-def mailing():
-    msg = Message('Hello', sender='jaggudixit686@gmail.com', recipients=['jagmohandixit686@gmail.com'])
-    mail.send(msg)
-
 @app.route('/')
-# @app.route('/about')
 def home():
     return render_template('index.html')
 
-@app.route('/home')
-def homes():
-    return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/form_mail', methods = ['POST'])
+def form_mail():
+    email = request.form['email']
+    mail_type = request.form['mail_type']
 
-@app.route('/services')
-def services():
-    return render_template('services.html')
+    if mail_type == "subcription-mail":
+        message = "Thank you subscribing to our newsletter"
+        name = request.form['name']
+        subject = request.form['subject']
+        user_message = request.form['message']
+    else:
+        message = "Your message has been sent. Thank you!"
 
-@app.route('/portfolio')
-def portfolio():
-    return render_template('portfolio.html')
-
-@app.route('/team')
-def team():
-    return render_template('team.html')
-
-@app.route('/dropdown')
-def dropdown():
+    server = SMTP(host='smtp.gmail.com', port=587)
+    server.starttls()
+    server.login('emailId', 'pass')
+    server.sendmail('naiktanvi30@gmail.com', email, message)
+    server.close()
     return render_template('index.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/testinomial')
-def testinomial():
-    return render_template('testinomial.html')
-
-@app.route('/form_mail', methods=['POST'])
-def form_mail():
-    name = request.form['name']
-    email = request.form['email']
-    subject = request.form['subject']
-    message = request.form['message']
-    s = smtplib.SMTP(host='smtp.gmail.com', port=587)
-    s.ehlo()
-    s.starttls()
-    s.login('jaggudixit686@gmail.com', 'jaggu@5252')
-    s.sentmail('jaggudixit686@gmail.com', 'jagmohandixit686@gmail.com', message)
-    s.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
