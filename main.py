c452c6522003e96326b6a506dcf6ba63fa342608
Jagmohan Dixit <78152773+Jagmohan-Dixit@users.noreply.github.com>
2022-01-26 15:54:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from flask import Flask, render_template, request, url_for
-from smtplib import SMTP
+
+def send_email(user, pwd, recipient, subject, body):
+    import smtplib
+
+    FROM = user
+    TO = recipient if isinstance(recipient, list) else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+
+    with smtplib.SMTP_SSL(host="mail.stackx.online") as smtp:
+        smtp.login(user,pwd)
+        smtp.sendmail(user,TO,message)
+        smtp.quit()
 
 app = Flask(__name__)
 
@@ -12,24 +28,19 @@ def home():
 def form_mail():
     email = request.form['email']
     mail_type = request.form['mail_type']
-    server = SMTP(host='smtp.gmail.com', port=587)
-    server.starttls()
-    server.login('naiktanvi30@gmail.com', '')
     if mail_type == "query-mail":
-        message = "Thankyou for your query we will contact you as son as possible !"
-        name = request.form['name']
-        subject = request.form['subject']
-        user_message = request.form['message']
-        server.sendmail('naiktanvi30@gmail.com', email, message)
-        server.sendmail(email, 'naiktanvi30@gmail.com', user_message)
+        subject = request.form['subject'] 
+        message = request.form['message']
+        send_email("info@stackx.online", "StackX@123", email, "Thankyou for contacting us","Our representative will reach to you soon.\nTeam Stackx")
+        
     else:
-        message = "Thank you for subscribing to our newsletter"
-        msg = "This email subscribe our newsletter !"
-        server.sendmail(email, 'naiktanvi30@gmail.com', msg)
-        server.sendmail('naiktanvi30@gmail.com', email, message)
+        send_email("info@stackx.online", "StackX@123", email, "ThankYou", "Thankyou For subscribing NewsLetter")
 
-    server.close()
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 if __name__ == '__main__':
